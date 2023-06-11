7e53be0a35a58fc783b87c523a259645c149e8b3
jdabassett <jacobbassett@gmail.com>
2023-06-10 23:47:16 -0600

    small changed to optionsHandler by jacob

--- modules/recipeHandler.js
+++ modules/recipeHandler.js
@@ -7,7 +7,6 @@ const recipeHandler = async (req, res) => {
         const { id } = req.query;
         const url = `http://themealdb.com/api/json/v2/1/lookup.php?i=${id}`;
         const results = await axios.get(url);
-        // console.log(results.data.meals[0]);
         const rawData = new FormattedResponse(results.data.meals[0]);
         const formattedData = rawData.returnObj();
         res.status(200).send(formattedData);
@@ -22,16 +21,16 @@ module.exports = recipeHandler;
 class FormattedResponse {
     constructor(resObj) {
         this.idMeal = resObj.idMeal,
-            this.strMeal = resObj.srtMeal,
-            this.strCategory = resObj.strCategory,
-            this.strArea = resObj.strArea,
-            this.strInstructions = resObj.strInstructions,
-            this.strMealThumb = resObj.strMealThumb,
-            this.strTags = resObj.strTags,
-            this.strYoutube = resObj.strYoutube;
-            this.rawRecipe = resObj;
-            this.ingredients = [];
-            // this.measurements = [];
+        this.strMeal = resObj.srtMeal,
+        this.strCategory = resObj.strCategory,
+        this.strArea = resObj.strArea,
+        this.strInstructions = resObj.strInstructions,
+        this.strMealThumb = resObj.strMealThumb,
+        this.strTags = resObj.strTags,
+        this.strYoutube = resObj.strYoutube;
+        this.rawRecipe = resObj;
+        this.ingredients = [];
+        // this.measurements = [];
     }
     getArr() {
         let ingredients = [];
@@ -41,9 +40,6 @@ class FormattedResponse {
             const concatIng = `${measurement} ${ingredient}`; 
             if (ingredient && ingredient !== '' && ingredient !== 'null') {
                 ingredients.push(concatIng);
-                // if (measurement && measurement.trim() !== '') {
-                //     this.measurements.push(measurement);
-                // }
             } else {
                 break;
             }
@@ -63,7 +59,7 @@ class FormattedResponse {
             strYoutube: this.strYoutube,
             recipeArr: this.recipeArr,
             ingredients: this.getArr()
-        }
+        };
     }
 }
 
